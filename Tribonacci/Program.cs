dabb7de1da4f4b4e7b3193dcddf3ca7c729a9ad9
Fredrik Johansson <fredrik.johansson@decerno.se>
2019-01-21 23:13:18 +0100

    First commit

---
+++ Tribonacci/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tribonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TribonacciSolver solver = new TribonacciSolver();
+            while (true)
+            {
+                Console.WriteLine("Ange hur lång tribonacci-sekvens du vill ha (skriv exit för att avsluta): ");
+                string line = Console.ReadLine();
+                if (line == "exit")
+                {
+                    break;
+                }
+                int input;
+                if (!int.TryParse(line, out input))
+                {
+                    Console.WriteLine("Felaktig input. Skriv ett naturligt tal eller 'exit'.");
+                    continue;
+                }
+                List<BigInteger> tribonacciSequence = solver.GetTribonacciSequence(input);
+                if (tribonacciSequence == null)
+                {
+                    Console.WriteLine("Felaktig input. Skriv ett naturligt tal eller 'exit'.");
+                    continue;
+                }
+                for (int i = 0; i < tribonacciSequence.Count; i++)
+                {
+                    Console.WriteLine(string.Format("f({0}) = {1}", i, tribonacciSequence[i]));
+                }
+            }
+        }
+    }
+}
---
+++ Tribonacci/TribonacciSolver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Tribonacci
+{
+    public class TribonacciSolver
+    {
+        public TribonacciSolver()
+        {
+
+        }
+
+        public List<BigInteger> GetTribonacciSequence(int sequenceLength)
+        {
+            try
+            {
+                List<BigInteger> seq = new List<BigInteger>();
+                if (sequenceLength <= 0)
+                {
+                    return null;
+                }
+                if (sequenceLength > 0)
+                {
+                    seq.Add(0);
+                }
+                if (sequenceLength > 1)
+                {
+                    seq.Add(0);
+                }
+                if (sequenceLength > 2)
+                {
+                    seq.Add(1);
+                }
+                for (int i = 3; i < sequenceLength; i++)
+                {
+                    seq.Add(seq[i - 1] + seq[i - 2] + seq[i - 3]);
+                }
+                return seq;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Ett fel inträffade medan tribonacci-sekvensen räknades ut. Error: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
+
